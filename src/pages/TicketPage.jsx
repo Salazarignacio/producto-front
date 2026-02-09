835a9cc78471185c30388c02f7d8b65c0972e5dc
ignacio salazar <ignaciosalazar986@gmail.com>
2026-02-09 20:31:44 -0300

    btns

--- src/pages/TicketPage.jsx
+++ src/pages/TicketPage.jsx
@@ -8,12 +8,11 @@ export default function TicketPage({ total, items }) {
       <li>Cantidad de productos: {items}</li>
       <li>Total ${total}</li>
       <div>
-        <Button className="btn-edit" onClick={handlePrint}>
+        <Button className="btn-edit  w-100" onClick={handlePrint}>
           Confirmar / Imprimir
         </Button>
-      </div>
-      <div>
-        <Button className="btn-edit">Cancelar Venta</Button>
+
+        <Button className="btn-edit  w-100 ">Cancelar Venta</Button>
       </div>
     </div>
   );
