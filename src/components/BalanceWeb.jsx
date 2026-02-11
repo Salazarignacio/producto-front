59821420ad6ddbb87ebf2bf3fa3eb8300f937254
ignacio salazar <ignaciosalazar986@gmail.com>
2026-02-11 19:01:08 -0300

    balance

--- src/components/BalanceWeb.jsx
+++ src/components/BalanceWeb.jsx
@@ -2,7 +2,7 @@ export default function BalanceWeb() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <iframe
-        src="https://github.com/"
+        src="https://master--exquisite-raindrop-96b2b7.netlify.app"
         title="Balance"
         style={{
           width: "100%",
