fd51ad6a28248d2049a46c53afbc0725a5f2392d
ignacio salazar <ignaciosalazar986@gmail.com>
2026-02-11 16:51:39 -0300

    btndelete

--- src/components/DeleteProductBDC.jsx
+++ src/components/DeleteProductBDC.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { destroy } from "../api/ProductoService";
 import { Button } from "react-bootstrap";
 import { ProductContext } from "../context/ProductContext";
-import deleteBtn from "../assets/deleteBtn.png";
+import deleteBtn from "../assets/btndelete.png";
 
 
 export default function DeleteProductoBDC({ id }) {
