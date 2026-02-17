9d10222222beea7118c0bcf644982982214bfa6b
Ignacio <ignaciosalazar986@gmail.com>
2026-02-17 12:50:31 -0300

    boolean para habilidar / deshabilitar boton edicion multiple

--- src/components/ModalUpdatePlural.jsx
+++ src/components/ModalUpdatePlural.jsx
@@ -5,13 +5,14 @@ import { useContext } from "react";
 
 export default function UpdatePlural({ ids }) {
   const { selectedProducts } = useContext(SelectedProducts);
+  const isEmpty = selectedProducts.length < 2;
 
   return (
     <>
       <Button
         variant="primary"
-        className="btn-mas"
-        onClick={() => console.log(selectedProducts)}
+        className={isEmpty ? "btn-vacio" : "btn-mas"}
+        disabled={isEmpty}
       >
         Edición Múltiple <img src={editbtn}></img>
       </Button>
