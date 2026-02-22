242be8659bbaf35d90a6513edf13c2fe953ed1f1
ignacio salazar <ignaciosalazar986@gmail.com>
2026-02-22 19:28:55 -0300

    edicion de precios

--- src/pages/VentasPage.jsx
+++ src/pages/VentasPage.jsx
@@ -10,7 +10,6 @@ export default function VentasPage({
     <div className="ticket-print">
       {props.map((a) => (
         <div key={a.codigo} className="carrito-container">
-        
           <li>{a.articulo}</li>
 
           {/* Input que cambia valor manualmente */}
@@ -33,19 +32,38 @@ export default function VentasPage({
                   soloNumeros ? Number(soloNumeros) : 0,
                 );
               }}
+                onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.target.blur();
+                }
+              }}
             />
+            
           </li>
 
           <li>
-            <span className="only-print">{a.cantidad}</span>
+            <span className="only-print">
+              {a.cantidad.toLocaleString("es-AR")}
+            </span>
+
             <input
               className="no-print sell-input"
-              type="number"
-              min="1"
-              value={a.cantidad}
-              onChange={(e) =>
-                actualizarCantidad(a.codigo, Number(e.target.value))
-              }
+              type="text"
+              inputMode="numeric"
+              value={a.cantidad ? a.cantidad.toLocaleString("es-AR") : ""}
+              onChange={(e) => {
+                const soloNumeros = e.target.value.replace(/\D/g, "");
+
+                actualizarCantidad(
+                  a.codigo,
+                  soloNumeros ? Math.max(1, Number(soloNumeros)) : 1,
+                );
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.target.blur();
+                }
+              }}
             />
           </li>
           <li className="no-print">${a.precio * a.cantidad}</li>
