554f520ccf00037f6db278c1f15099c6fa4ccfbb
Ignacio <ignaciosalazar986@gmail.com>
2026-02-23 09:18:13 -0300

    bug print

--- src/pages/TicketPage.jsx
+++ src/pages/TicketPage.jsx
@@ -15,6 +15,7 @@ export default function TicketPage({ total, items, setProductos }) {
   };
   const handlePrint = () => {
     window.print();
+     if (!ok) return;
     setProductos([]);
     localStorage.removeItem("productos");
   };
